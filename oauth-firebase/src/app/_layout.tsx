73a516247586fe5a96d5044f5bd16fe50c640be0
Robert Sajdok <robert.sajdok@gmail.com>
2025-03-28 14:27:29 +0100

    remove old files

--- oauth-firebase/src/app/_layout.tsx
+++ oauth-firebase/src/app/_layout.tsx
@@ -1,12 +1,14 @@
 
 import '../../global.css';
 import { View, Text } from "react-native";
+import GoogleSignInButton from './components/GoogleSignInButton';
 
 export default function AppLayout() {
 
     return (
         <View className="flex-1 items-center justify-center">
-            <Text className="text-cyan-800 text-2xl">App Layout</Text>
+            <Text className="text-yellow-300 text-2xl">App Layout</Text>
+            <GoogleSignInButton />
         </View>
     )
 }
