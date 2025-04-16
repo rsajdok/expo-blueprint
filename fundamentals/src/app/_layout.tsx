e74cc4e973fead09bc71afef7f17377bda6be6de
Robert Sajdok <robert.sajdok@gmail.com>
2025-04-16 10:25:21 +0200

    add new project 'fundamentals'

---
+++ fundamentals/src/app/_layout.tsx
@@ -0,0 +1,8 @@
+import "../../global.css"
+import { Slot } from "expo-router"
+
+export default function RootLayout() {
+    return (
+        <Slot />
+    );
+}
---
+++ fundamentals/src/app/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+export default function App() {
+    return (
+        <Text className="text-2xl">Welcome to app!</Text>
+    );
+}
