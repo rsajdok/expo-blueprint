49f30b2799d4d9133f41cdbc0df8813562f515e8
Robert Sajdok <robert.sajdok@gmail.com>
2025-04-16 11:16:10 +0200

    fundamentals: useLocalSearchParams

--- fundamentals/src/app/index.tsx
+++ fundamentals/src/app/index.tsx
@@ -3,11 +3,11 @@ import { Button, Text, View } from 'react-native';
 
 export default function IndexScreen() {
     return (
-        <View className="flex-1 items-center justify-center bg-white p-4">
+        <View className="flex-1 items-center justify-center bg-white gap-6">
             <Text className="text-2xl">Welcome to app!</Text>
-            <Link href="/second" push asChild>
+            <Link href={{ pathname: "/second", params: { name: "Ris" } }} push asChild >
                 <Button title="Second Screen" />
-            </Link>
-        </View>
+            </Link >
+        </View >
     );
 }
--- fundamentals/src/app/second.tsx
+++ fundamentals/src/app/second.tsx
@@ -1,10 +1,13 @@
 import { View, Text, Button } from "react-native";
-import { Link } from "expo-router";
+import { Link, useLocalSearchParams } from "expo-router";
 
 export default function SecondScreen() {
+    const params = useLocalSearchParams<{ name?: string }>();
+
     return (
-        <View className="flex-1 items-center justify-center bg-white p-4">
+        <View className="flex-1 items-center justify-center bg-white gap-4">
             <Text className="text-2xl">Second Screen</Text>
+            <Text className="text-lg">Name: {params.name}</Text>
             <Link href="/third" push asChild>
                 <Button title="Third Screen" />
             </Link>
